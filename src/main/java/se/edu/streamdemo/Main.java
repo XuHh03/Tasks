a08ccf7dbfff847a933e1a3e100b6b045be0fd94
Huanghao <andrewxu1688@gmail.com>
2025-09-24 01:48:49 +0800

    add more code for working with streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -15,10 +15,15 @@ public class Main {
         System.out.println("Printing all data ...");
         printAllData(tasksData);
 
+        printDataUsingStreams(tasksData);
+
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
 
+        printDeadlinesUsingStreams(tasksData);
+
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (using streams): " + countDeadlinesUsingStreams(tasksData));
 
     }
 
@@ -41,9 +46,16 @@ public class Main {
     }
 
     public static void printAllData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using iterations ...");
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using streams ...");
+        tasks.stream()
+                .forEach(System.out::println);
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
